647bc27f8ecb0d077f57d0e1719a0310f5e6c288
ArmyofDeux <103246856+Emmanuetoks@users.noreply.github.com>
2023-05-16 14:12:40 -0700

    CREATE SLIDE BUTTON
    Created the slide button. The angle icons are background images

--- src/components/SideText.jsx
+++ src/components/SideText.jsx
@@ -1,12 +1,30 @@
+import arrowImg from '../../public/assets/images/icon-arrow.svg'
+import angleLeft from '../../public/assets/images/icon-angle-left.svg'
+
+import angleRight from '../../public/assets/images/icon-angle-right.svg'
+
+
 const SideText = ({ children, heading, bodyText }) => {
     return (
-        <div className='main__side-text grid center-items bg-white'>
+        <div className='main__side-text grid center-items bg-white pos-rel'>
             <article>
                 <h1 className="fw-500 clr-black">{heading}</h1>
-                <p className="fw-200 text-accent-100" style={{'marginBlock': '2rem'}}>{bodyText}</p>
+                <p className="fw-200 text-accent-100" style={{ 'marginBlock': '2rem' }}>{bodyText}</p>
                 {children}
-            </article> 
-
+                <a className="fw-300 clr-black flex shop-now uppercase" style={{ 'letterSpacing': '10px', 'cursor': 'pointer', 'alignItems': 'center', '--gap': '1rem' }}>shop now
+                    <span>
+                        <img src={arrowImg} />
+                    </span>
+                </a>
+            </article>
+            <div className='slider-bar flex pos-abs bg-black' style={{'--gap': 'rem'}}>
+                <button className='slide-btn bg-black btn-left'>
+                    <span className='sr-only'>btn right</span>
+                </button>
+                <button className='slide-btn bg-black btn-right'>
+                    <span className='sr-only'>btn left</span>
+                </button>
+            </div>
         </div>
     )
 }
