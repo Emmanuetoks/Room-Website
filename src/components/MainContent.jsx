e89756389210cc1fd8e04941ac7252cc12c16894
ArmyofDeux <103246856+Emmanuetoks@users.noreply.github.com>
2023-05-17 16:33:47 -0700

    ADD SLIDE LOGIC
    Added the slide logic by conditionally changing state of variable 'position'

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -4,17 +4,35 @@ import { useSideText } from "../hooks/myhooks"
 const MainContent = () => {
   const [position, setPosition] = useState(0)
   const { slide1, slide2, slide3 } = useSideText()
+  const slideData = [slide1, slide2, slide3]
+
+  const slideBack = () => {
+    console.log(position);
+    if (position != 0) {
+      setPosition(prevValue => prevValue - 1)
+    } else {
+      setPosition(slideData.length - 1)
+    }
+  }
+
+  const slideFoward = () => {
+    if (position != slideData.length - 1) {
+      setPosition(prevValue => prevValue + 1)
+    }else {
+      setPosition (0)
+    }
+  }
 
   return (
     <div className='main__content grid'>
       <picture>
-        <source srcSet={'hello'} media="(max-width: 500px)" />
-        <img className="hero-image" src={slide1.desktopImg} />
+        <source srcSet={slideData[position].mobileImg} media="(max-width: 500px)" />
+        <img className="hero-image" src={slideData[position].desktopImg} />
       </picture>
       <div className='main__side-text grid center-items bg-white pos-rel'>
         <article>
-          <h1 className="fw-500 clr-black">{'hello world'}</h1>
-          <p className="fw-200 text-accent-100" style={{ 'marginBlock': '2rem' }}>{'come on'}</p>
+          <h1 className="fw-500 clr-black">{slideData[position].heading}</h1>
+          <p className="fw-200 text-accent-100" style={{ 'marginBlock': '2rem' }}>{slideData[position].paragraphText}</p>
           <a className="fw-300 clr-black flex shop-now uppercase" style={{ 'letterSpacing': '10px', 'cursor': 'pointer', 'alignItems': 'center', '--gap': '1rem' }}>shop now
             <span>
               <img src={'gg'} />
@@ -23,17 +41,14 @@ const MainContent = () => {
         </article>
         <div className='slider-bar flex pos-abs bg-black' style={{ '--gap': 'rem' }}>
 
-          <a className='slide-link'>
-            <button className='slide-btn bg-black btn-left'>
-              <span className='sr-only'>btn right</span>
-            </button>
-          </a>
+          <button onClick={slideBack} className='slide-btn bg-black btn-left'>
+            <span className='sr-only'>btn right</span>
+          </button>
 
-          <a className='slide-link'>
-            <button className='slide-btn bg-black btn-right'>
-              <span className='sr-only'>btn left</span>
-            </button>
-          </a>
+          <button onClick={slideFoward} className='slide-btn bg-black btn-right'>
+            <span className='sr-only'>btn left</span>
+          </button>
+
         </div>
       </div>
     </div>
--- src/hooks/myhooks.jsx
+++ src/hooks/myhooks.jsx
@@ -2,24 +2,23 @@ const useSideText = () => {
     const homeText = {
         slide1: {
             heading: 'Discover innovative ways to decorate',
-
             paragraphText: 'We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to lif. Create a room in your own style with our collection and make your property a reflection of you and what you love.',
             mobileImg: '../../assets/images/mobile-image-hero-1.jpg',            
             desktopImg: '../../assets/images/desktop-image-hero-1.jpg',
         },
 
         slide2: {
-            heading: 'hello world',
+            heading: 'We are available all across the globe',
             mobileImg: '../../assets/images/mobile-image-hero-2.jpg',
             desktopImg: '../../assets/images/desktop-image-hero-2.jpg',
-            paragraphText: 'we are here'
+            paragraphText: "With stores all over the world, it's easy for you to find durniture for your home or place of business. Locally, we're in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today."
         },
 
         slide3: {
-            heading: 'contact',
+            heading: 'Manufactured with the best materials',
             mobileImg: '../../assets/images/mobile-image-hero-3.jpg',
             desktopImg: '../../assets/images/desktop-image-hero-3.jpg',
-            paragraphText: 'contact text'
+            paragraphText: 'Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible, With three decaded of experience in this industry, we understand what customers want for their home and office'
         },
 
     }
